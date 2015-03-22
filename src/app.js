9df75a6469cb375c0426216ee9995a01655abb65
Matt Venables <mattvenables@gmail.com>
2015-03-22 17:50:37 -0400

    Add morgan package for logging

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@
 try { require('dotenv').load(); } catch(e) {}
 var express = require('express');
 var helmet = require('helmet');
+var morgan = require('morgan');
 var app = express();
 
 app.use(helmet());
+app.use(morgan('dev'));
 
 app.get('/', function(req, res) {
   res.json({});
