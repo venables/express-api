5896340120804685068e12fdcb79ceb17d282003
Matt Venables <mattvenables@gmail.com>
2015-03-22 17:45:45 -0400

    Add helmet for security headers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var express = require('express');
+var helmet = require('helmet');
 var app = express();
+
+app.use(helmet());
 
 app.get('/', function(req, res) {
   res.json({});
